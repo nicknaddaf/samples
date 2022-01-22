13cb6f1c0896c95ed0b4317478b5793d91f53079
Nick Naddaf <nick.naddaf@gmail.com>
2022-01-22 00:21:22 -0500

    add node simple API

---
+++ node-simple-api/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
